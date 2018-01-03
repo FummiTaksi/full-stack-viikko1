abd4a111d14ed10147d1b252540f34c40a5c9915
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-03 15:00:46 +0200

    t26 tehty

--- src/puhelinluettelo/Puhelinluettelo.js
+++ src/puhelinluettelo/Puhelinluettelo.js
@@ -12,20 +12,45 @@ class Puhelinluettelo extends React.Component {
       }
     }
   
+
+
+    addPerson = (e) => {
+        e.preventDefault();
+        const newPerson = {name: this.state.newName}
+        const persons = this.state.persons.concat(newPerson);
+        this.setState({
+            persons: persons,
+            newName: ''
+        })
+    }
+
+    handleNoteChange = (e) => {
+        this.setState({ newName: e.target.value })
+    }
+
     render() {
+      const persons = this.state.persons.map((person) => {
+          return <li key = {person.name}>{person.name}</li>;
+      })
       return (
         <div>
           <h2>Puhelinluettelo</h2>
-          <form>
+          <form onSubmit={this.addPerson}>
             <div>
-              nimi: <input />
+              nimi:
+              <input
+                value={this.state.newName}
+                onChange={this.handleNoteChange}
+               />
             </div>
             <div>
               <button type="submit">lisää</button>
             </div>
           </form>
           <h2>Numerot</h2>
-          ...
+          <ul>
+            {persons}
+          </ul>    
         </div>
       )
     }
